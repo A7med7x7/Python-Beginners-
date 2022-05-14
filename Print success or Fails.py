c9a49a58b3a38f4008e1b10a3bd97cd72d4b6f41
Ahmed Alghali <95593096+A7med7x7@users.noreply.github.com>
2022-05-15 01:23:40 +0200

    Add files via upload
    Python Beginners Project That Prints Success Or Fails

---
+++ Print success or Fails.py
@@ -0,0 +1,9 @@
+#a code that prints success or fails
+grade = input() # input function used to collect the grade info
+if (grade >= 50):
+    print ("passed")
+#if the grade is greater than or equal to 50 the program will print "passed"
+else:
+   print ("fails")
+# if the grade is less than 50 the program will print "fails"
+
