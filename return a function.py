76da2dc632e5a3b48e5ddc7a8556f6978ceda3c1
Ahmed Alghali <95593096+A7med7x7@users.noreply.github.com>
2022-07-04 23:45:58 +0200

    Function Example in Python
    here is a simple program that returns a value, functions in python

---
+++ return a function.py
@@ -0,0 +1,7 @@
+# this is a simple program that returns the funtion value
+def f(x): #firstly you put the def (define) functoin and the the name of it the parameter is always inside the brackets
+    return x*2 #you ask the fuction to return a value or you will not get a value
+result = f(5) #store the value in a variable and then print it
+print (result)
+
+#A7
